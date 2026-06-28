1ced0fc9b773eaa36fbe34846a3d56bbe8229477
Quastrado <Ataxyswriter@yandex.ru>
2026-06-28 17:34:59 +0300

    bad test

---
+++ src/test/java/tests/WdHubStatusTests.java
@@ -0,0 +1,20 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+public class WdHubStatusTests extends TestBase {
+    @Test
+    public void statusTest() {
+        given()
+                .log().all()
+                .auth().basic("user1", "1234")
+                .get("/wd/hub/status")
+                .then()
+                .log().all()
+                .body("value.message", is("Selenoid v2.1.0 built at 2026-06-28_12:20:34AM"));
+
+    }
+}
